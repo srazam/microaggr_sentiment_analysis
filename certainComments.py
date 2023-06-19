1623a7b6df5f524e9d3f270ceba39b122b21b38e
srazam <srazam5@gmail.com>
2023-06-19 09:31:21 -0400

    Update certainComments.py

--- certainComments.py
+++ certainComments.py
@@ -1,57 +1,55 @@
 import csv
-from googleapiclient.discovery import build
+import googleapiclient.discovery
+import googleapiclient.errors
 
-api_key = "AIzaSyDGH2Sddkb-KLDSWErXWEVCJzx4d42EfTU"
-youtube = build('youtube', 'v3', developerKey=api_key)
+def get_video_comments(api_key, video_id, max_results):
+    # Create the YouTube API client
+    youtube = googleapiclient.discovery.build('youtube', 'v3', developerKey=api_key)
 
-def get_video_comments(video_id, max_results):
-    comments = []
-    next_page_token = None
-
-    while True:
+    try:
+        # Retrieve the comments using the YouTube API
         response = youtube.commentThreads().list(
-            part="snippet",
+            part='snippet',
             videoId=video_id,
-            maxResults=min(max_results, 100),
-            pageToken=next_page_token
+            maxResults=max_results,
+            order='time'  # Retrieve comments in chronological order
         ).execute()
 
+        # Extract the relevant information from the API response
+        comments = []
         for item in response['items']:
             comment = item['snippet']['topLevelComment']['snippet']
-            author = comment['authorDisplayName']
-            text = comment['textDisplay']
-            published_at = comment['publishedAt']
-
             comments.append({
-                'author': author,
-                'text': text,
-                'published_at': published_at,
-                'video_id': video_id
+                'video_id': video_id,
+                'author': comment['authorDisplayName'],
+                'text': comment['textDisplay'],
+                'published_at': comment['publishedAt'],
+                'comment_id': item['id'],
+                'parent_id': item['snippet']['parentId']
             })
 
-        next_page_token = response.get('nextPageToken')
+        return comments
 
-        if not next_page_token or len(comments) >= max_results:
-            break
-
-    return comments
+    except googleapiclient.errors.HttpError as e:
+        print(f'An HTTP error {e.resp.status} occurred: {e.content}')
 
 def save_comments_to_csv(comments, filename):
-    with open(filename, 'w', newline='', encoding='utf-8') as csvfile:
-        fieldnames = ['author', 'text', 'published_at', 'video_id']
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    # Define the CSV fieldnames
+    fieldnames = ['video_id', 'author', 'text', 'published_at', 'comment_id', 'parent_id']
 
+    # Write the comments to a CSV file
+    with open(filename, 'w', newline='', encoding='utf-8') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
         writer.writerows(comments)
 
-# Video ID of the YouTube video
-video_id = "wUn05hdkhjM"
-
-# Maximum number of comments to retrieve
+# Set your API key, video ID, and maximum number of comments to retrieve
+api_key = 'AIzaSyDGH2Sddkb-KLDSWErXWEVCJzx4d42EfTU'
+video_id = 'wUn05hdkhjM'
 max_results = 18
 
-# Retrieve comments
-comments = get_video_comments(video_id, max_results)
+# Retrieve the comments
+comments = get_video_comments(api_key, video_id, max_results)
 
-# Save comments to a CSV file
+# Save the comments to a CSV file
 save_comments_to_csv(comments, 'comments.csv')
