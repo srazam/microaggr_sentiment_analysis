74cc70ac8622577bdd57fc27b09825ca5c39a323
srazam <srazam5@gmail.com>
2023-06-19 11:11:47 -0400

    Extract highest number of comments needed

--- preprocess.py
+++ preprocess.py
@@ -1,5 +1,16 @@
-import csv
+csv_file = [("Sarah", 28, "A"), 
+        ("Sam", 32, "B"), 
+        ("Jean", 21, "C"),
+        ]
 
-#Put name of csv here
-input_file = csv.DictReader(open("nameof.csv"))
+for row in csv_file:
+    for text in row:
+        text.lower()
+        if text == "ok" or text == "hi" or text == "yes":
+            del text
+        if text.isnumeric():
+            del text
 
+        for char in text:
+            if char == "." or char == "!" or char == ",":
+                del text
