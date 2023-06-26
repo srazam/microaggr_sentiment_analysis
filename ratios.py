e689ece20151a581d95345ad234799cb42506bc1
srazam <srazam5@gmail.com>
2023-06-26 11:03:27 -0400

    Got views to comment ratios for each trailer

---
+++ ratios.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+#Read csv file into a pandas DataFrame
+df = pd.read_csv(r'C:\Users\AzamF\Documents\GitHub\reuData\Superhero Movie Trailer Statistics - Sheet2.csv')
+
+#Calculate ratio of views to comments
+df['Ratio'] = df['viewCount'] / df['commentCount']
+
+#Save the DataFram with the added column back the the same CSV file
+df.to_csv(r'C:\Users\AzamF\Documents\GitHub\reuData\Superhero Movie Trailer Statistics - Sheet2.csv', index=False)
