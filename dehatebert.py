1e21a6bbb73a8100612c272c9a3985ae0d826d31
srazam <srazam5@gmail.com>
2023-07-03 16:14:59 -0400

    Updating the use of the model

--- dehatebert.py
+++ dehatebert.py
@@ -2,23 +2,18 @@ from transformers import pipeline
 import pandas as pd
 import re
 
-#Put name of file in the input_file 
-input_file = r"C:\Users\AzamF\Desktop\trailerData\AntMan_6-27.csv"
-output_file = r"C:\Users\AzamF\Documents\GitHub\reuData\modelResults\labeled_WW1984.csv"
+input_file = r"C:\Users\AzamF\Documents\GitHub\reuData\trailerData\Aquaman_6-27.csv"
+output_file = r"C:\Users\AzamF\Documents\GitHub\reuData\modelResults\labeled_Aquaman.csv"
 
 data=pd.read_csv(input_file)
-data["Comment Text"] = data["Comment Text"].str.lower()
-data["Comment Text"] = data["Comment Text"].apply(lambda x: re.sub(r'[^\w\s]', '', x))
+
 texts = data['Comment Text'].tolist()
 
-# Step 1: Load the model
 model_name = "Hate-speech-CNERG/dehatebert-mono-english"
 classifier = pipeline("text-classification", model=model_name)
 
-# Step 2: Classify text
 results = classifier(texts)
 
-# Step 3: Interpret the result
 output_data=[]
 for text, result in zip(texts, results):
     label = result["label"]
