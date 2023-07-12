1227471e5b1355269fd46022fd8b3f0df30f5b80
srazam <srazam5@gmail.com>
2023-07-11 22:11:43 -0400

    Update wordCloud.py

--- wordCloud.py
+++ wordCloud.py
@@ -1,19 +1,43 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from wordcloud import WordCloud
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+import nltk
+nltk.download('stopwords')
+nltk.download('punkt')
 
 text_data = ' '
 
-files = [r'something.csv',
-         r'somethingelse.csv']
+files = [r'C:\Users\AzamF\Documents\GitHub\reuData\trueLabels\labeled_AntMan - labeled_AntMan.csv',
+         r'C:\Users\AzamF\Documents\GitHub\reuData\trueLabels\labeled_Aquaman - labeled_Aquaman.csv',
+         r'C:\Users\AzamF\Documents\GitHub\reuData\trueLabels\labeled_BlackAdam - labeled_BlackAdam.csv',
+         r'C:\Users\AzamF\Documents\GitHub\reuData\trueLabels\labeled_BlackPanther.csv',
+         r'C:\Users\AzamF\Documents\GitHub\reuData\trueLabels\labeled_CaptainMarvel.csv',
+         r'C:\Users\AzamF\Documents\GitHub\reuData\trueLabels\labeled_CAWinterSoldier - labeled_CAWinterSoldier.csv',
+         r'C:\Users\AzamF\Documents\GitHub\reuData\trueLabels\labeled_DoctorStrange - labeled_DoctorStrange.csv',
+         r'C:\Users\AzamF\Documents\GitHub\reuData\trueLabels\labeled_MsMarvel - labeled_MsMarvel.csv',
+         r'C:\Users\AzamF\Documents\GitHub\reuData\trueLabels\labeled_Pennyworth - labeled_Pennyworth.csv',
+         r'C:\Users\AzamF\Documents\GitHub\reuData\trueLabels\labeled_ShangChi - labeled_ShangChi.csv',
+         r'C:\Users\AzamF\Documents\GitHub\reuData\trueLabels\labeled_SHAZAM - labeled_SHAZAM.csv',
+         r'C:\Users\AzamF\Documents\GitHub\reuData\trueLabels\labeled_WonderWoman - labeled_WonderWoman.csv']
 
-#load data from the csv file
-data = pd.read_csv('thefile.csv')
 
-#generate the word cloud from a specific column
-for index, row in data.iterrows():
-    if data['True Label'] == 'HATE':
-        text_data += row['Column Text'] + ' '
+for file in files:
+    #load data from the csv file
+    data = pd.read_csv(file)
+
+    #generate the word cloud from a specific column
+    for index, row in data.iterrows():
+        if row['True Label'] == 'HATE':
+            text_data += row['Comment Text'] + ' '
+
+#Remove stop words
+stop_words = set(stopwords.words('english'))
+tokens = word_tokenize(text_data)
+filtered_text = [word for word in tokens if word.lower() not in stop_words]
+filtered_text = ' '.join(filtered_text)
 
 wordcloud = WordCloud(width=800, height=400).generate(text_data)
 
