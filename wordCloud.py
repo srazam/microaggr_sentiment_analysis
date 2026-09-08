1cbf8a906d2d329cd730e5f1835505d48d60c169
srazam <srazam5@gmail.com>
2026-09-07 21:00:41 -0400

    Update direct paths to relative paths, add comments, and save word cloud

--- wordCloud.py
+++ wordCloud.py
@@ -1,48 +1,51 @@
+'''
+    Creating a word cloud of common words found in all comments labeled as HATE
+'''
+
 import pandas as pd
 import matplotlib.pyplot as plt
 from wordcloud import WordCloud
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
-
 import nltk
+from pathlib import Path
+
+# Downloading resources for tokenizing text and accessing stopword list 
 nltk.download('stopwords')
 nltk.download('punkt')
+nltk.download('punkt_tab')
 
+files = [Path('.') / 'trueLabels' / filename for filename in
+         ['labeled_AntMan.csv',
+          'labeled_Aquaman.csv',
+          'labeled_BlackAdam.csv',
+          'labeled_BlackPanther.csv',
+          'labeled_CaptainMarvel.csv',
+          'labeled_CAWinterSoldier.csv',
+          'labeled_DoctorStrange.csv',
+          'labeled_MsMarvel.csv',
+          'labeled_Pennyworth.csv',
+          'labeled_ShangChi.csv',
+          'labeled_SHAZAM.csv',
+          'labeled_WonderWoman.csv']]
+
+# Getting all words from hate comments
 text_data = ' '
-
-files = [r'C:\Users\AzamF\Documents\GitHub\reuData\trueLabels\labeled_AntMan - labeled_AntMan.csv',
-         r'C:\Users\AzamF\Documents\GitHub\reuData\trueLabels\labeled_Aquaman - labeled_Aquaman.csv',
-         r'C:\Users\AzamF\Documents\GitHub\reuData\trueLabels\labeled_BlackAdam - labeled_BlackAdam.csv',
-         r'C:\Users\AzamF\Documents\GitHub\reuData\trueLabels\labeled_BlackPanther.csv',
-         r'C:\Users\AzamF\Documents\GitHub\reuData\trueLabels\labeled_CaptainMarvel.csv',
-         r'C:\Users\AzamF\Documents\GitHub\reuData\trueLabels\labeled_CAWinterSoldier - labeled_CAWinterSoldier.csv',
-         r'C:\Users\AzamF\Documents\GitHub\reuData\trueLabels\labeled_DoctorStrange - labeled_DoctorStrange.csv',
-         r'C:\Users\AzamF\Documents\GitHub\reuData\trueLabels\labeled_MsMarvel - labeled_MsMarvel.csv',
-         r'C:\Users\AzamF\Documents\GitHub\reuData\trueLabels\labeled_Pennyworth - labeled_Pennyworth.csv',
-         r'C:\Users\AzamF\Documents\GitHub\reuData\trueLabels\labeled_ShangChi - labeled_ShangChi.csv',
-         r'C:\Users\AzamF\Documents\GitHub\reuData\trueLabels\labeled_SHAZAM - labeled_SHAZAM.csv',
-         r'C:\Users\AzamF\Documents\GitHub\reuData\trueLabels\labeled_WonderWoman - labeled_WonderWoman.csv']
-
-
 for file in files:
-    #load data from the csv file
     data = pd.read_csv(file)
-
-    #generate the word cloud from a specific column
     for index, row in data.iterrows():
         if row['True Label'] == 'HATE':
             text_data += row['Comment Text'] + ' '
 
-#Remove stop words
+#Removing stop words
 stop_words = set(stopwords.words('english'))
 tokens = word_tokenize(text_data)
 filtered_text = [word for word in tokens if word.lower() not in stop_words]
 filtered_text = ' '.join(filtered_text)
 
 wordcloud = WordCloud(width=800, height=400).generate(text_data)
-
-#display word cloud
 plt.figure(figsize=(10,5))
 plt.imshow(wordcloud, interpolation='bilinear')
 plt.axis('off')
+plt.savefig('finalGraphs/wordCloud.png')
 plt.show()
